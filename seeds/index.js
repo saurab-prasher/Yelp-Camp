f8faf702668fbc1442e160923c88c01d59587c8a
Saurab Prasher <saurab015@gmail.com>
2021-01-01 13:22:49 +0530

    partials added

--- seeds/index.js
+++ seeds/index.js
@@ -22,9 +22,14 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20 + 10);
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://source.unsplash.com/collection/483251",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, molestias. Sed ipsa accusantium aut in alias est natus cum corrupti molestias quas! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam quis necessitatibus cupiditate ullam fugiat. Voluptatum quis assumenda, ea nisi dolor vel, voluptatem voluptatibus sint at nostrum suscipit sequi officia? Quis! Consequatur, et dolores!",
+      price,
     });
     await camp.save();
   }
