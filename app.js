62334d8703e828a6fe69bcc26e120fd30fb9ff31
Saurab Prasher <saurab015@gmail.com>
2021-01-03 21:44:01 +0530

    comments added

--- app.js
+++ app.js
@@ -127,11 +127,11 @@ app.delete(
 );
 
 // ERROR HANDLER
-
 app.all("*", (req, res, next) => {
   next(new ExpressError("Page Not Found"), 404);
 });
 
+// Error handling
 app.use((err, req, res, next) => {
   const { statusCode = 500 } = err;
   if (!err.message) err.message = "Something went wrong";
