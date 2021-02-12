1fc071054802a022bda051230050ecc192f084fa
Saurab Prasher <saurab015@gmail.com>
2021-02-12 20:48:05 +0530

    readme updated

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Yelp-Camp
+
+# Demo--link
+
+[Click here to see the demo site](https://shielded-spire-61735.herokuapp.com/)
+--> may take some time to load
--- routes/users.js
+++ routes/users.js
@@ -21,6 +21,43 @@ router
     users.login
   );
 
+router.post("/changepassword", function (req, res) {
+  User.findOne({ _id: "your id here" }, (err, user) => {
+    // Check if error connecting
+    if (err) {
+      res.json({ success: false, message: err }); // Return error
+    } else {
+      // Check if user was found in database
+      if (!user) {
+        res.json({ success: false, message: "User not found" }); // Return error, user was not found in db
+      } else {
+        user.changePassword(
+          req.body.oldpassword,
+          req.body.newpassword,
+          function (err) {
+            if (err) {
+              if (err.name === "IncorrectPasswordError") {
+                res.json({ success: false, message: "Incorrect password" }); // Return error
+              } else {
+                res.json({
+                  success: false,
+                  message:
+                    "Something went wrong!! Please try again after sometimes.",
+                });
+              }
+            } else {
+              res.json({
+                success: true,
+                message: "Your password has been changed successfully",
+              });
+            }
+          }
+        );
+      }
+    }
+  });
+});
+
 // logout user
 router.get("/logout", users.logout);
 
